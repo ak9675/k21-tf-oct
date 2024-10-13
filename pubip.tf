62f7e76365e6cb309d7d4c2759b065de0d36ed6c
akhil <akhil@gmail.com>
2024-10-13 17:27:37 +0000

    rg is configured

--- pubip.tf
+++ pubip.tf
@@ -1,14 +1,15 @@
 resource "azurerm_public_ip" "day8rg" {
   allocation_method       = "Static"
-  location                = data.azurerm_resource_group.datarg.location
+  location                = azurerm_resource_group.myrg.location
   name                    = var.user-inp
-  resource_group_name     = data.azurerm_resource_group.datarg.name
+  resource_group_name     = azurerm_resource_group.myrg.name
 
 }
 
-data "azurerm_resource_group" "datarg" {
-name = "finalrg"
-} 
+resource "azurerm_resource_group" "myrg" {
+  name = "finalday8rg"
+  location = "eastus"
+}
 
 variable "user-inp" {
   description = "Enter name for public ip for terraform cloud"
